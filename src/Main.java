c3d884472baefacf6af075e560b3a2926a83b61d
FelipeOthon <felipeosantiago@gmail.com>
2022-10-15 16:24:46 -0300

    enviar main java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por
+        //hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas
+        //decimais
+
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        int numero;
+        double horasTrabalhadas, valorHoras, salario;
+
+
+
+        System.out.printf("Digite o numero do funcionario: ");
+        numero = scanner.nextInt();
+
+        System.out.printf("Digite qual é o valor da hora trabalhada: ");
+        valorHoras = scanner.nextDouble();
+
+        System.out.printf("Digite o numero de horas trabalhadas: ");
+        horasTrabalhadas = scanner.nextDouble();
+
+        salario = horasTrabalhadas * valorHoras;
+
+        System.out.printf("O funcionario do numero %.1f, trabalhou %.2f e seu salario será %.2f", numero, horasTrabalhadas, salario );
+
+
+
+        scanner.close();
+
     }
 }
